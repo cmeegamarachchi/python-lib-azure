19958f0cb29ec8bda0252cddbf05a187811b0455
Chintana Meegamarachchi <chintana@appsolve.com.au>
2024-11-16 16:02:58 +0000

    Debug

--- api.azure/test/__init__.py
+++ api.azure/test/__init__.py
@@ -3,7 +3,7 @@ import azure.functions as func  # type: ignore
 import sys
 import os
 
-sys.path.append(os.path.join(os.path.dirname(__file__), '../../core.lib'))
+# sys.path.append(os.path.join(os.path.dirname(__file__), '../../core.lib'))
 
 # this code is required to import the utility class from the core.lib when running in azure
 sys.path.append(os.path.join(os.path.dirname(__file__), './core.lib'))
