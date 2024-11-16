904fc3560dc6b81eb5f6bc473f5de744046fc875
Chintana Meegamarachchi <chintana@appsolve.com.au>
2024-11-16 15:13:34 +0000

    Adds utility code

--- api.azure/test/__init__.py
+++ api.azure/test/__init__.py
@@ -1,5 +1,15 @@
 import azure.functions as func  # type: ignore
 
+import sys
+import os
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '../../core.lib'))
+
+# Import the Utility class
+from utility import Utility
+
 def main(req: func.HttpRequest) -> func.HttpResponse:
+    utility = Utility()
     name = req.params.get("name")
-    return func.HttpResponse(f"Hello {name}")
+    message = utility.generate_greeting(name)
+    return func.HttpResponse(f"Hello {message}")
